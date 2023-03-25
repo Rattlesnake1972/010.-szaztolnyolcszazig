a45dd850db44df3594c11877831079e075193453
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 21:28:30 +0100

    100-tól 800-ig írja ki a számokat

---
+++ 010. szaztolnyolcszazig/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010.szaztolnyolcszazig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i < 801; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
